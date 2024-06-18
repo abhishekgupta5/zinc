f5184e44a6784dc19d9c6904255c66074be2c05c
Abhishek <zenofabhishek@gmail.com>
2024-06-18 22:46:50 +0530

    Initial commit

---
+++ lib/zinc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "httparty"
+require "zinc/client"
+require_relative "zinc/version"
+
+module Zinc
+end
---
+++ lib/zinc/client.rb
@@ -0,0 +1,7 @@
+module Zinc
+  class Client
+    include HTTParty
+    base_uri "https://api.zinc.io/v1/"
+
+  end
+end
