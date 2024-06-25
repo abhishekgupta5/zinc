051328f994a7750a9b1c3db6443177ea731ce1ec
Abhishek <zenofabhishek@gmail.com>
2024-06-25 05:33:24 +0530

    Add orders and products' APIs

---
+++ lib/zinc/configuration.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Zinc
+  class << self
+    extend Forwardable
+
+    def_delegators :configuration, :client_token, :client_token=
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
+  # Alias for backward compatibility
+  def self.config
+    configuration
+  end
+
+  class Configuration
+    attr_accessor :client_token
+
+    def initialize
+      @client_token = nil
+    end
+  end
+end
---
+++ lib/zinc/error.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Zinc
+  class Error < StandardError
+    attr_reader :response
+
+    def initialize(response)
+      @response = response.parsed_response
+      super(error_message)
+    end
+
+    private
+
+    def error_message
+      "#{response.dig("error", "code") || response["code"]}:
+       #{response.dig("error", "message") || response["message"]}"
+    end
+  end
+end
